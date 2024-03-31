a503b9d37c5a60bc1ee994b8709e6b1c8e81b7f0
dmkng <themaking64@cock.li>
2024-03-31 12:40:30 +0200

    Improved config

--- main.ps1
+++ main.ps1
@@ -1,4 +1,11 @@
-$Id = 'HID\VID_2386&PID_432F&COL02\6&2AE36198&0&0001'
+# Device ID (can be found in Device Manager under Properties > Details > Hardware Ids)
+$Id = 'HID\VID_2386&PID_432F&COL02\6&2AE36198&0&0001' # Lenovo ThinkPad T480 Touch Screen
+
+# Name displayed in the notification
+$Name = 'Touch screen'
+
+# Icon displayed in the notification
+$Icon = 'screen'
 
 ##################################################
 
@@ -15,44 +22,44 @@ $Xml = New-Object Windows.Data.Xml.Dom.XmlDocument
 $Notify = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe')
 
 while($true) {
-    $Item = Get-PnpDevice -InstanceId $Id -Class 'HIDClass'
-    if($Item.Status -eq 'OK') {
-	$WaitId = (Start-Process -PassThru -NoNewWindow -RedirectStandardOutput NUL waitfor DeviceSwitcher$PID).Id
-        Set-Content .wait.pid $WaitId
-        Wait-Process $WaitId
+	$Item = Get-PnpDevice -InstanceId $Id
+	if($Item.Status -eq 'OK') {
+		$WaitId = (Start-Process -PassThru -NoNewWindow -RedirectStandardOutput NUL waitfor DeviceSwitcher$PID).Id
+		Set-Content .wait.pid $WaitId
+		Wait-Process $WaitId
 
-        Disable-PnpDevice $Id -Confirm:$false
+		Disable-PnpDevice $Id -Confirm:$false
 
-        $Xml.LoadXml(@"
+		$Xml.LoadXml(@"
 <toast>
-    <audio silent='true'/>
-    <visual>
-        <binding template='ToastGeneric'>
-            <text>Touch screen has been disabled.</text>
-            <image placement='appLogoOverride' src='$($PSScriptRoot)\icon-off.png'/>
-        </binding>
-    </visual>
+	<audio silent='true'/>
+	<visual>
+		<binding template='ToastGeneric'>
+			<text>$Name has been disabled.</text>
+			<image placement='appLogoOverride' src='$($PSScriptRoot)\$($Icon)-off.png'/>
+		</binding>
+	</visual>
 </toast>
 "@)
-        $Notify.Show([Windows.UI.Notifications.ToastNotification]::new($Xml))
-    } elseif($Item.Status -eq 'Error') {
-	$WaitId = (Start-Process -PassThru -NoNewWindow -RedirectStandardOutput NUL waitfor DeviceSwitcher$PID).Id
-        Set-Content .wait.pid $WaitId
-        Wait-Process $WaitId
+		$Notify.Show([Windows.UI.Notifications.ToastNotification]::new($Xml))
+	} elseif($Item.Status -eq 'Error') {
+		$WaitId = (Start-Process -PassThru -NoNewWindow -RedirectStandardOutput NUL waitfor DeviceSwitcher$PID).Id
+		Set-Content .wait.pid $WaitId
+		Wait-Process $WaitId
 
-        Enable-PnpDevice $Id -Confirm:$false
+		Enable-PnpDevice $Id -Confirm:$false
 
-        $Xml.LoadXml(@"
+		$Xml.LoadXml(@"
 <toast>
-    <audio silent='true'/>
-    <visual>
-        <binding template='ToastGeneric'>
-            <text>Touch screen has been enabled.</text>
-            <image placement='appLogoOverride' src='$($PSScriptRoot)\icon-on.png'/>
-        </binding>
-    </visual>
+	<audio silent='true'/>
+	<visual>
+		<binding template='ToastGeneric'>
+			<text>$Name has been enabled.</text>
+			<image placement='appLogoOverride' src='$($PSScriptRoot)\$($Icon)-on.png'/>
+		</binding>
+	</visual>
 </toast>
 "@)
-        $Notify.Show([Windows.UI.Notifications.ToastNotification]::new($Xml))
-    }
+		$Notify.Show([Windows.UI.Notifications.ToastNotification]::new($Xml))
+	}
 }
